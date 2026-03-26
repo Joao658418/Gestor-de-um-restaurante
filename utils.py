80788cc11ebc2660784e8c420c41b323b4f1dedb
Joao658418 <joaosouza.13827@ae-danielsampaio.pt>
2026-03-26 15:39:19 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
+# Listas e Dicionários globais que funcionam como nosso Banco de Dados
+db_clientes = {}  # Usamos dicionário: {NIF: {dados}}
+db_pratos = []    # Usamos lista: [{dados}, {dados}]
 
+def limpar_dados():
+    global db_clientes, db_pratos
+    db_clientes.clear()
+    db_pratos.clear()
+    print("Dados limpos com sucesso!")
