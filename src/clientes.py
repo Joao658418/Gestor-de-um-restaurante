9e3c225beebbe3aff2d4e03bec7c702599d800d7
Joao658418 <joaosouza.13827@ae-danielsampaio.pt>
2026-04-15 09:38:37 +0100

    Update clientes.py

--- src/clientes.py
+++ src/clientes.py
@@ -1,35 +1,73 @@
 from utils import db_clientes
 
+
 def adicionar_cliente(nif, nome, nascimento, telefone):
-    if nif in db_clientes:
-        print(f"\n❌ Erro: O NIF {nif} já existe!")
-    else:
-        db_clientes[nif] = {
+    try:
+        if not nif or not nome:
+            return 400, "NIF e Nome são obrigatórios!"
+
+        if nif in db_clientes:
+            return 400, "Este NIF já está registado!"
+        cliente = {
             "nome": nome,
             "nascimento": nascimento,
-            "telefone": telefone
+            "telefone": telefone,
+            "nif": nif
         }
-        print(f"\n✅ Cliente '{nome}' adicionado com sucesso.")
+        db_clientes[nif] = cliente
+        return 201, cliente
+
+    except Exception as e:
+        # Se acontecer algum erro bizarro no Python, simulamos o erro 500
+        return 400, f"Erro interno do sistema: {e}"
+
 
 def atualizar_cliente(nif, novo_nome, novo_nasc, novo_tel):
-    if nif in db_clientes:
-        db_clientes[nif].update({
+    try:
+        if nif not in db_clientes:
+            return 404,  "Cliente não encontrado!"
+        cliente = {
             "nome": novo_nome,
             "nascimento": novo_nasc,
-            "telefone": novo_tel
-        })
-        print(f"\n✅ Cliente com NIF {nif} atualizado com sucesso!")
-    else:
-        print("\n❌ Erro: Cliente não encontrado.")
+            "telefone": novo_tel,
+            "nif": nif
+        }
+        db_clientes[nif].update(cliente)
+        return 200, cliente
+
+    except Exception as e:
+        return 400,"Erro interno: {e}"
+
 
 def remover_cliente(nif):
-    if nif in db_clientes:
-        # O método pop remove a chave do dicionário
-        eliminado = db_clientes.pop(nif)
-        print(f"\n🗑️ Cliente '{eliminado['nome']}' removido com sucesso!")
-    else:
-        print("\n❌ Erro: Cliente não encontrado.")
+    try:
+        if nif not in db_clientes:
+            return 404, "Cliente não encontrado!"
+
+        cliente_eliminado = db_clientes.pop(nif)
+        return 200, cliente_eliminado
+
+    except Exception as e:
+        return 500, f"Erro interno: {e}"
+
 
 def pesquisar_cliente(nif):
-    # Retorna os dados se encontrar, ou None se não encontrar
-    return db_clientes.get(nif, None)
+    try:
+        if nif in db_clientes:
+            return 200, db_clientes[nif]
+        return 404,"Cliente não encontrado!",
+
+    except Exception as e:
+        return 500, "Erro interno: {e}"
+
+
+def listar_clientes():
+    if not db_clientes:
+        print("\n[HTTP 404] Não existem clientes registados.")
+        return 404, "Não existem clientes registados."
+
+    print("\n--- LISTAGEM DE CLIENTES ---")
+    for nif, dados in db_clientes.items():
+        print(f"NIF: {nif} | Nome: {dados['nome']} | Data Nasc: {dados['nascimento']} | Tel: {dados['telefone']}")
+
+    return 200, "Sucesso"
