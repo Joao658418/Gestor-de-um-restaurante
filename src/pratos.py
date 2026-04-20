0b595c934e2a78e809147d3535ecb5a4eee323f4
Joao658418 <joaosouza.13827@ae-danielsampaio.pt>
2026-04-20 12:19:47 +0100

    Update pratos.py

--- src/pratos.py
+++ src/pratos.py
@@ -18,7 +18,7 @@ def criar_prato(nome, preco, estrelas, tipo, ingredientes):
             "ingredientes": ingredientes
         }
         db_pratos.append(novo_prato)
-        return 201, "Prato criado com sucesso."
+        return 201, novo_prato
     except Exception as e:
         return 500, str(e)
 
@@ -28,16 +28,14 @@ def listar_pratos():
         return 404, "Não existem pratos no cardápio."
 
     print("\n--- CARDÁPIO ---")
-    for p in db_pratos:
-        print(f"ID: {p['id']} | Nome: {p['nome']} | Preço: {p['preco']}€ | Tipo: {p['tipo']}")
-    return 200, "Sucesso"
+    return 200, db_pratos
 
 
 def consultar_prato(id_prato):
     for p in db_pratos:
         if p["id"] == id_prato:
             print(f"\n🔍 Detalhes: {p}")
-            return 200, "Sucesso"
+            return 200, p
     return 404, "Prato não encontrado."
 
 
@@ -49,13 +47,13 @@ def atualizar_prato(id_prato, nome=None, preco=None, estrelas=None, tipo=None, i
             if estrelas: p["estrelas"] = estrelas
             if tipo: p["tipo"] = tipo
             if ingredientes: p["ingredientes"] = ingredientes
-            return 200, "Atualizado"
+            return 200, p
     return 404, "Prato não encontrado."
 
 
 def remover_prato(id_prato):
     for i, p in enumerate(db_pratos):
         if p["id"] == id_prato:
-            db_pratos.pop(i)
-            return 200, "Removido"
+            prato = db_pratos.pop(i)
+            return 200, prato
     return 404, "Prato não encontrado."
